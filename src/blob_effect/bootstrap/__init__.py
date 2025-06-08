ba0728731b055a4764a0d4c0691c5e65239fb767
KodaiYamashita <nglcobdai@gmail.com>
2025-06-09 08:03:31 +0900

    fix: Update messenger initialization to use Path for message.ini

--- src/blob_effect/bootstrap/__init__.py
+++ src/blob_effect/bootstrap/__init__.py
@@ -1,7 +1,8 @@
+from pathlib import Path
 from nglcobdai_utils import Messenger, ConsoleHandlerInfo, Settings, get_logger
 
 settings = Settings()
 
 logger = get_logger(settings.PROJECT_NAME, ch_info=ConsoleHandlerInfo())
 
-messenger = Messenger("src/blob_effect/config/message.ini")
+messenger = Messenger(Path(__file__).parent.parent / "config" / "message.ini")
