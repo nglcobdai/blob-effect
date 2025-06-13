ad8389241adcbcebaff9b123b35ea68b77e0c354
KodaiYamashita <nglcobdai@gmail.com>
2025-06-14 08:42:54 +0900

    fix: update logger initialization to use __name__ instead of PROJECT_NAME

--- src/blob_effect/bootstrap/__init__.py
+++ src/blob_effect/bootstrap/__init__.py
@@ -3,6 +3,6 @@ from nglcobdai_utils import Messenger, ConsoleHandlerInfo, Settings, get_logger
 
 settings = Settings()
 
-logger = get_logger(settings.PROJECT_NAME, ch_info=ConsoleHandlerInfo())
+logger = get_logger(__name__, ch_info=ConsoleHandlerInfo())
 
 messenger = Messenger(Path(__file__).parent.parent / "config" / "message.ini")
